0d377a1deca39f4c43d9f5b3b22c1d2b50624cc4
Aiman <ahardao1001@g.educaand.es>
2024-04-23 19:35:28 +0200

    falta arreglarlo

--- src/main/java/daw/PanelPrincipal.java
+++ src/main/java/daw/PanelPrincipal.java
@@ -16,7 +16,7 @@ import javax.swing.JTextArea;
  *
  * @author aiman
  */
-public class PanelPrincipal extends JPanel {
+public class PanelPrincipal extends JPanel implements ActionListener{
 
     // Atributos de la clase (privados)
     private PanelBotones botonera;
@@ -44,5 +44,80 @@ public class PanelPrincipal extends JPanel {
         // Colocamos la botonera y el área texto
         this.add(areaTexto, BorderLayout.NORTH);
         this.add(botonera, BorderLayout.SOUTH);
+        
+        for (JButton jb : botonera.getgrupoBotones()) {
+            jb.addActionListener(this);
+        }
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        double valorActual = 0;
+        Object o = e.getSource();
+        if (o instanceof JButton jButton) {
+            String textoBoton = jButton.getText();
+            switch (textoBoton) {
+                case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" -> {
+                    areaTexto.setText(areaTexto.getText() + textoBoton );
+                }
+                case "+" -> {
+                    valor = Double.parseDouble(areaTexto.getText());
+                    tipoOperacion = 0;
+                    areaTexto.setText(Double.parseDouble(areaTexto.getText())+"+"+valor);
+                }
+                case "-" -> {
+                    valor = Double.parseDouble(areaTexto.getText());
+                    tipoOperacion = 1;
+                    areaTexto.setText("");
+                }
+                case "*" -> {
+                    valor = Double.parseDouble(areaTexto.getText());
+                    tipoOperacion = 2;
+                    areaTexto.setText(valor+"");
+                }
+                case "/" -> {
+                    valor = Double.parseDouble(areaTexto.getText());
+                    tipoOperacion = 3;
+                    areaTexto.setText(valor+"");
+                }
+                case "C" -> {
+                    tipoOperacion = -1;
+                    areaTexto.setText(valor+"");
+                }
+                case "=" -> {
+                    try {
+                        valorActual = Double.parseDouble(areaTexto.getText());
+                        switch (tipoOperacion) {
+                            case 0 -> {
+                                // Suma
+                                double resultadoSuma = valor + valorActual;
+                                areaTexto.setText(String.valueOf(valor+"+"+valorActual+"="+resultadoSuma));
+                            }
+                            case 1 -> {
+                                // Resta
+                                double resultadoResta = valor - valorActual;
+                                areaTexto.setText(String.valueOf(valor+"+"+valorActual+"="+resultadoResta));
+                            }
+                            case 2 -> {
+                                // Multiplicación
+                                double resultadoMultiplicacion = valor * valorActual;
+                                areaTexto.setText(String.valueOf(valor+"+"+valorActual+"="+resultadoMultiplicacion));
+                            }
+                            case 3 -> {
+                                // División
+                                if (valorActual != 0) {
+                                    double resultadoDivision = valor / valorActual;
+                                    areaTexto.setText(String.valueOf(valor+"+"+valorActual+"="+resultadoDivision));
+                                } else {
+                                    areaTexto.setText("Error: división por cero");
+                                }
+                            }
+                        }
+                    } catch (NumberFormatException ex) {
+                        areaTexto.setText("Error: entrada inválida");
+                    }
+                    tipoOperacion = -1;
+                }
+            }
+        }
     }
 }
